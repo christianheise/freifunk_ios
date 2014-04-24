7566d43740c9f6040b6a4b84d0ba41183bdd5636
Christian Heise <christian.heise@gmail.com>
2014-04-24 08:32:11 +0200

    Update region.rb
    Freifunk Lüneburg hinzugefügt

--- app/01_models/region.rb
+++ app/01_models/region.rb
@@ -4,6 +4,7 @@ class Region < Struct.new(:key, :name, :zoom, :location, :data_url, :twitter, :h
     Region.new(:jena,       "Jena",          9, [50.90, 11.60], "http://map.freifunk-jena.de/ffmap/nodes.json",          "freifunkjena",     "http://freifunk-jena.de/"),
     Region.new(:kiel,       "Kiel",          9, [54.30, 10.10], "http://freifunk.in-kiel.de/ffmap/nodes.json",           "freifunkkiel",     "http://freifunk.in-kiel.de/"),
     Region.new(:luebeck,    "Lübeck",        8, [53.80, 10.70], "http://freifunk.metameute.de/map/nodes.json",           "freifunkluebeck",  "http://freifunk.metameute.de/"),
+    Region.new(:lueneburg,  "Lüneburg",     10, [53.24, 10.42], "http://freifunk-lueneburg.de/karte/nodes.json",         "freifunkLG",       "http://freifunk-lueneburg.de"),
     Region.new(:paderborn,  "Paderborn",     9, [51.70,  8.75], "http://map.paderborn.freifunk.net/nodes.json",          "FreifunkPB",       "http://paderborn.freifunk.net/"),
     Region.new(:wuppertal,  "Wuppertal",     9, [51.20,  7.15], "http://map.freifunk-wuppertal.net/nodes.json",          "ffwtal",           "http://freifunk-wuppertal.net/"),
     Region.new(:dresden,    "Dresden",       9, [51.08, 13.73], "http://info.ddmesh.de/info/nodes.json",                 "ddmesh",           "http://www.ddmesh.de/"),
